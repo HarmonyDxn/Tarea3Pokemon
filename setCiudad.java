c43f06dd968cf48a0d0dd4eabcb5ae617db56e46
HarmonyDxn <71037944+HarmonyDxn@users.noreply.github.com>
2020-12-11 17:51:05 -0300

    Delete setCiudad.java

--- setCiudad.java
+++
@@ -1,4 +0,0 @@
-public class setCiudad {
-    public static final String [] nombres = {"Pueblo Paleta","Ciudad Verde","Ciudad Plateada","Ciudad Celeste","Ciudad Azafrán","Ciudad Carmín","Ciudad Azulona","Ciudad Fucsia","Pueblo Lavanda","Isla Canela"};
-
-}
