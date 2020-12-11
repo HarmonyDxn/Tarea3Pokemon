657a898e7e2f6585310ffd930f9703909644b672
HarmonyDxn <71037944+HarmonyDxn@users.noreply.github.com>
2020-12-11 02:16:40 -0300

    Add files via upload

---
+++ setCiudad.java
@@ -0,0 +1,4 @@
+public class setCiudad {
+    public static final String [] nombres = {"Pueblo Paleta","Ciudad Verde","Ciudad Plateada","Ciudad Celeste","Ciudad Azafrán","Ciudad Carmín","Ciudad Azulona","Ciudad Fucsia","Pueblo Lavanda","Isla Canela"};
+
+}
